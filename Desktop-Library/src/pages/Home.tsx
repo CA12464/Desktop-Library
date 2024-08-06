777b1287d1ad3f78ff1f0c67b69206ee69103bfc
CA12464 <connoradams298@gmail.com>
2024-08-06 13:14:02 -0500

    new home page

--- Desktop-Library/src/pages/Home.tsx
+++ Desktop-Library/src/pages/Home.tsx
@@ -1,16 +1,21 @@
 // src/pages/Home.js
-import './Home.css';
+import '../Background_Stylings/Neon_Sign.css'; // Import the CSS file
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to The Home Library</h1>
-      <p>Explore your book collection and manage your library efficiently.</p>
-      <ul>
-      </ul>
+    <div className="hero">
+      <div className="hero-wrapper">
+        <div className="hero-block">
+          <h1 className="hero-title">
+            <span>Ms. Lady's</span>
+            <span>Mystic Volumes</span>
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Home;
+
 
